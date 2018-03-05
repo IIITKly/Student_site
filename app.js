269a70723585fbc0e79ed5a5375618bc5bc09c33
mevk_7 <vikrantkarn007@gmail.com>
2018-03-05 14:03:14 +0530

    Added stylesheet

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
-
-
 var app = express();
+app.use(express.static(__dirname +"/public"));
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.set('view engine','ejs');
