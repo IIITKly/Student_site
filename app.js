612ad4c29703d679f4dfa642cd7a3b3a17f253c6
mevk_7 <vikrantkarn007@gmail.com>
2018-03-05 13:04:53 +0530

    Adding landing page

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.set('view engine','ejs');
+app.use(methodOverride('_method'));
+
+app.get('/',function(req,res){
+
+	res.send("landing page");
+});
+
+app.listen(3000,function(){
+	console.log("server started");
+})
